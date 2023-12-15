59f024f6d1be9b8b4734f8399c00e849e34d3000
AleksandrLednyov <lednyov@mail.ru>
2023-12-15 11:23:37 +0300

    added constructor, bubble sort

--- Demo.java
+++ Demo.java
@@ -1,15 +1,17 @@
-package lesson04;
+package myArrayList;
+
+import java.util.Comparator;
 
 public class Demo {
     public static void main(String[] args) {
         LASArrayList<Cat> list = new LASArrayList<>();
         System.out.println(list);
 
-        list.add(new Cat("Барсик"));
-        list.add(new Cat("Матроскин"));
-        list.add(new Cat("Бегемот"));
-        list.add(new Cat("Ричи"));
-        list.add(new Cat("Арчи"));
+        list.add(new Cat("Барсик", 8));
+        list.add(new Cat("Матроскин", 12));
+        list.add(new Cat("Бегемот", 15));
+        list.add(new Cat("Ричи", 2));
+        list.add(new Cat("Арчи", 5));
         System.out.println(list);
 
         list.remove(2);
@@ -18,24 +20,41 @@ public class Demo {
         System.out.println(list.get(2));
 
         LASArrayList<Cat> list2 = new LASArrayList<>();
-        list2.add(new Cat("Симба"));
-        list2.add(new Cat("Нала"));
+        list2.add(new Cat("Симба", 3));
+        list2.add(new Cat("Нала", 3));
         System.out.println(list2);
 
         list.addAll(list2);
         System.out.println(list);
+
+        Comparator<Cat> compareCats = new CompareCatsByAge();
+        LASArrayList.LASBubbleSort(list, compareCats);
+        System.out.println(list);
+
+        LASArrayList<Cat> list3 = new LASArrayList<>(list2);
+        System.out.println(list3);
     }
 }
 
-class Cat {
+class Cat{
     String name;
+    int age;
 
-    public Cat(String name) {
+    public Cat(String name, int age) {
         this.name = name;
+        this.age = age;
     }
 
     @Override
     public String toString() {
-        return name;
+        return name + " возраст " + age;
+    }
+}
+
+class CompareCatsByAge implements Comparator<Cat> {
+
+    @Override
+    public int compare(Cat o1, Cat o2) {
+        return o1.age - o2.age;
     }
 }
--- LASArrayList.java
+++ LASArrayList.java
@@ -1,14 +1,27 @@
-package lesson04;
+package myArrayList;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class LASArrayList<E> {
     private E[] elements;
-    private final int DEFAULT_CAPACITY = 5;
+    private static final int DEFAULT_CAPACITY = 5;
     private int size;
 
     public LASArrayList () {
         elements = (E[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public LASArrayList (int capacity) {
+        if (capacity >= 0)
+            elements = (E[]) new Object[capacity];
+        else throw new IllegalArgumentException("capacity < 0");
+    }
+
+    public LASArrayList (LASArrayList<E> list) {
+        this.elements = (E[]) new Object[list.size];
+        this.size = list.size;
+        System.arraycopy(list.elements, 0, this.elements, 0, list.size);
     }
 
     public int size() {
@@ -38,14 +51,18 @@ public class LASArrayList<E> {
         elements = newElements;
     }
 
-    public Object get(int index){
-        return elements[index];
+    public E get(int index){
+        return (E) elements[index];
+    }
+
+    public void set(int index, E element) {
+        elements[index] = element;
     }
 
     public void remove(int index) {
         System.arraycopy(elements, index+1, elements, index, size-index-1);
-        elements[size-1] = null;
-        size--;
+        elements[--size] = null;
+//        size--;
     }
 
     public void addAll(LASArrayList<E> list) {
@@ -57,5 +74,24 @@ public class LASArrayList<E> {
     @Override
     public String toString() {
         return Arrays.toString(Arrays.copyOf(elements, size));
+    }
+
+    public static <E> void LASBubbleSort(LASArrayList<E> list, Comparator<E> comparator) {
+        E temp;
+        boolean changed = true;
+        int right = list.size - 1;
+
+        while (changed) {
+            changed = false;
+            for (int i = 0; i < right; i++) {
+                if (comparator.compare(list.get(i), list.get(i + 1)) > 0) {
+                    temp = list.get(i + 1);
+                    list.set(i + 1, list.get(i));
+                    list.set(i, temp);
+                    changed = true;
+                }
+            }
+            right--;
+        }
     }
 }
